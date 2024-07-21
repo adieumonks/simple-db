6326c740c4198ef96d2465a3afa43ccb1dd182f1
sudoken <44742486+adieumonks@users.noreply.github.com>
2024-07-21 13:46:58 +0900

    add scan test 2

--- query/scan_test.go
+++ query/scan_test.go
@@ -70,3 +70,77 @@ func TestScan1(t *testing.T) {
 	s4.Close()
 	tx.Commit()
 }
+
+func TestScan2(t *testing.T) {
+	db, _ := server.NewSimpleDB(path.Join(t.TempDir(), "scantest2"), 400, 8)
+	tx := db.NewTransaction()
+
+	sch1 := record.NewSchema()
+	sch1.AddIntField("A")
+	sch1.AddStringField("B", 9)
+	layout1 := record.NewLayoutFromSchema(sch1)
+	us1, _ := record.NewTableScan(tx, "T1", layout1)
+
+	n := 200
+
+	if err := us1.BeforeFirst(); err != nil {
+		t.Fatalf("failed to prepare for first")
+	}
+	t.Logf("inserting %d random records int T1.", n)
+	for i := 0; i < n; i++ {
+		us1.Insert()
+		us1.SetInt("A", int32(i))
+		us1.SetString("B", fmt.Sprintf("bbb%d", i))
+	}
+	us1.Close()
+
+	sch2 := record.NewSchema()
+	sch2.AddIntField("C")
+	sch2.AddStringField("D", 9)
+	layout2 := record.NewLayoutFromSchema(sch2)
+	us2, _ := record.NewTableScan(tx, "T2", layout2)
+	if err := us2.BeforeFirst(); err != nil {
+		t.Fatalf("failed to prepare for first")
+	}
+	t.Logf("inserting %d random records into T2.", n)
+	for i := 0; i < n; i++ {
+		us2.Insert()
+		us2.SetInt("C", int32(n-i-1))
+		us2.SetString("D", fmt.Sprintf("ddd%d", n-i-1))
+	}
+	us2.Close()
+
+	s1, _ := record.NewTableScan(tx, "T1", layout1)
+	s2, _ := record.NewTableScan(tx, "T2", layout2)
+	s3 := query.NewProductScan(s1, s2)
+
+	term := query.NewTerm(query.NewExpressionFromField("A"), query.NewExpressionFromField("C"))
+	pred := query.NewPredicateFromTerm(term)
+	t.Logf("the predicate is %v", pred)
+	s4 := query.NewSelectScan(s3, pred)
+
+	fields := []string{"B", "D"}
+	s5 := query.NewProjectScan(s4, fields)
+
+	next, err := s5.Next()
+	if err != nil {
+		t.Fatalf("failed to get next record: %v", err)
+	}
+	for next {
+		b, err := s5.GetString("B")
+		if err != nil {
+			t.Fatalf("failed to get string: %v", err)
+		}
+		d, err := s5.GetString("D")
+		if err != nil {
+			t.Fatalf("failed to get string: %v", err)
+		}
+		t.Logf("B: %s, D: %s", b, d)
+		next, err = s5.Next()
+		if err != nil {
+			t.Fatalf("failed to get next record: %v", err)
+		}
+	}
+	s5.Close()
+	tx.Commit()
+}
